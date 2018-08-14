c4209940735a824024ec4277f60d50a844b93739
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 03:53:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromDriverByKey(obj, key) {
-  const newObj = Object.assign({}, driver);
+  const newObj = Object.assign({}, obj);
   delete newObj[key];
   return newObj;
 }
